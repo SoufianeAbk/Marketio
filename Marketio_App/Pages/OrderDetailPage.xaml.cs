98d752b9c75c61605b7c18301e5b0bbfdae902cb
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2026-03-14 22:46:37 +0100

    Voeg bestellingdetailpagina toe met laadstatus en foutafhandeling
    - Implementeer OrderDetailPage met responsive grid layout
    - Voeg laad, fout en inhoudstoestanden toe met zichtbaarheid binding
    - Toon bestelgegevens: ordernummer, datum, status, betalingsmethode
    - Zet bestelde producten in CollectionView met totaalbedrag berekening
    - Voeg verzendadres en totaalbedrag weergave toe
    - Implementeer OnAppearing() om bestelling automatisch te laden
    - Vervang Label met Frame voor statusbadge (CornerRadius compatibiliteit)
    - Voeg vernieuwen- en terugknoppen toe in bottomnavigatie

--- Marketio_App/Pages/OrderDetailPage.xaml.cs
+++ Marketio_App/Pages/OrderDetailPage.xaml.cs
@@ -1,10 +1,23 @@
+using Marketio_App.ViewModels;
+
 namespace Marketio_App.Pages
 {
     public partial class OrderDetailPage : ContentPage
     {
-        public OrderDetailPage()
+        public OrderDetailPage(OrderDetailViewModel viewModel)
         {
             InitializeComponent();
+            BindingContext = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (BindingContext is OrderDetailViewModel viewModel)
+            {
+                await viewModel.LoadOrderCommand.ExecuteAsync(viewModel.OrderId);
+            }
         }
     }
 }
