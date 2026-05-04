61d0d28f1ae594b87d5498b46a9bf1c2f8a12911
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2026-05-04 10:37:45 +0200

    - Migrations "initiële EF-migratie toegevoegd voor WPF-databank"

---
+++ Marketio_WPF/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Marketio_WPF.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MarketioDbContext>
+    {
+        public MarketioDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MarketioDbContext>();
+
+            // Use the same connection string as in App.xaml.cs
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=MarketioDb;Integrated Security=true;";
+
+            optionsBuilder.UseSqlServer(
+                connectionString,
+                b => b.MigrationsAssembly("Marketio_WPF"));
+
+            return new MarketioDbContext(optionsBuilder.Options);
+        }
+    }
+}
