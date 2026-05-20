9c2f9fc869ee8d7328c776440abdfd01e16695ce
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2026-05-20 04:00:41 +0200

    ProductsView toegevoegd met XAML-layout en code-behind.

--- Marketio_WPF/Views/ProductsView.xaml.cs
+++ Marketio_WPF/Views/ProductsView.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Marketio_WPF.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Marketio_WPF.Views
 {
-    /// <summary>
-    /// Interaction logic for ProductsView.xaml
-    /// </summary>
     public partial class ProductsView : UserControl
     {
         public ProductsView()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ProductsViewModel vm)
+            {
+                vm.LoadProductsCommand.Execute(null);
+            }
         }
     }
 }
