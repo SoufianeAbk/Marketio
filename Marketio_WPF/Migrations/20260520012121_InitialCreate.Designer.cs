b0ca4c2ee86ba5323699e54289421f47d88feac5
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2026-05-20 04:00:11 +0200

    GETUTCDATE() vervangen door NOW() in InitialCreate-migratie voor PostgreSQL-compatibiliteit.

--- Marketio_WPF/Migrations/20260520012121_InitialCreate.Designer.cs
+++ Marketio_WPF/Migrations/20260520012121_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Marketio_WPF.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Marketio_WPF.Migrations
 {
     [DbContext(typeof(MarketioDbContext))]
-    partial class MarketioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260520012121_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
